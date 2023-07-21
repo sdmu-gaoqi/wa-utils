a709764b14ccf3912fbc97a4fe8fdc5984ab7c65
gaoqi <1224362143@qq.com>
2023-07-21 19:31:59 +0800

    chore: 添加axios

---
+++ src/axios/axios.ts
@@ -0,0 +1,35 @@
+import axios, {
+  AxiosHeaders,
+  AxiosInstance,
+  CreateAxiosDefaults,
+  RawAxiosRequestHeaders,
+} from 'axios';
+
+export enum RequestType {
+  form = 'multipart/form-data',
+  json = 'application/json; charset=utf-8',
+  htmlForm = 'application/x-www-form-urlencoded',
+  stream = 'application/octet-stream',
+}
+
+const defaultHeads: RawAxiosRequestHeaders | AxiosHeaders = {
+  'Content-Type': RequestType.json,
+};
+
+interface RequestOption extends CreateAxiosDefaults {
+  baseUrl: string;
+}
+
+export class request {
+  instence: AxiosInstance;
+  constructor(option: RequestOption) {
+    const { baseUrl, timeout = 5000 } = option;
+    this.instence = axios.create({
+      baseURL: baseUrl,
+      timeout,
+      headers: {
+        ...defaultHeads,
+      },
+    });
+  }
+}
