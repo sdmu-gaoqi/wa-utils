0b26a106489d9b3b87656db7b9ba08ba760972e4
gaoqi <1224362143@qq.com>
2023-07-31 19:57:21 +0800

    chore: 添加joinCss导出

--- src/joinCss/joinCss.ts
+++ src/joinCss/joinCss.ts
@@ -1,5 +1,7 @@
-export const joinCss = (prefixCls: string, names: string[]) => {
+const joinCss = (prefixCls: string, names: string[]) => {
   return names.map((name) => {
     return `${prefixCls}-${name}`;
   });
 };
+
+export default joinCss;
