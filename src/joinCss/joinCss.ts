87eff4200cec352104c8f18bba139f13fc91c0da
gaoqi <1224362143@qq.com>
2023-08-01 11:08:31 +0800

    chore: 修改joinCss

--- src/joinCss/joinCss.ts
+++ src/joinCss/joinCss.ts
@@ -1,7 +1,9 @@
 const joinCss = (prefixCls: string, names: string[]) => {
-  return names.map((name) => {
-    return `${prefixCls}-${name}`;
-  });
+  return names
+    .map((name) => {
+      return `${prefixCls}-${name}`;
+    })
+    .join(` `);
 };
 
 export default joinCss;
