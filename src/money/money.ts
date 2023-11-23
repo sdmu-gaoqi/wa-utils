3b6dc62863029df67bd6ce8d2956bf395c8c5b1d
gaoqi <1224362143@qq.com>
2023-11-23 22:34:24 +0800

    chore: 添加formatMoney

--- src/money/money.ts
+++ src/money/money.ts
@@ -2,4 +2,23 @@ function toThousands(num: string) {
   return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
 }
 
-export { toThousands };
+function formatMoney(value: any) {
+  if (!value) {
+    return '0';
+  }
+  // 将参数转换为浮点数
+  let floatValue = parseFloat(value || 0);
+
+  // 检查是否是有效的数值
+  if (isNaN(floatValue)) {
+    return 'Invalid value';
+  }
+
+  // 使用toFixed()方法将浮点数转换为字符串，并指定小数位数为2
+  let formattedValue = floatValue.toFixed(2);
+
+  // 返回格式化后的字符串
+  return formattedValue;
+}
+
+export { toThousands, formatMoney };
