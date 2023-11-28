1e0e3acbc42ab9874dd996aab05b0d90c84dc92a
gaoqi <1224362143@qq.com>
2023-11-28 11:00:34 +0800

    chore: 添加uuid formatMoney

---
+++ src/format/format.ts
@@ -0,0 +1,17 @@
+/**
+ * @param value 金额
+ * @param decimal 小数位
+ * @param thousands 千分位
+ * @desc 格式不正确返回的0
+ * */
+export const formatMoney = (value: number, decimal = 2, thousands = false) => {
+  if (!Number(value)) {
+    return 0;
+  } else {
+    const formattedValue = Number(value).toLocaleString();
+    const decimalStr = `.${'0'.repeat(decimal)}`;
+    return !thousands
+      ? `${Number(value)}${decimalStr}`
+      : `${formattedValue}${decimalStr}`;
+  }
+};
